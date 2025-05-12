2625ab45a3240d37b4171e04d76cd45daf15a86e
Mayada <mayadaelsayed383@gmail.com>
2025-05-12 15:48:22 +0300

    implement UserIcon component to display user profile image or default icon

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,5 +1,24 @@
-function UserIcon() {
-  return <div>UserIcon</div>;
+import { LuUser } from 'react-icons/lu';
+import { currentUser, auth } from '@clerk/nextjs/server';
+
+async function UserIcon() {
+  const { userId } = auth();
+
+  const user = await currentUser();
+
+  const profileImage = user?.imageUrl;
+
+  if (profileImage) {
+    return (
+      // eslint-disable-next-line @next/next/no-img-element
+      <img
+        src={profileImage}
+        alt='img'
+        className='w-6 h-6 rounded-full object-cover'
+      />
+    );
+  }
+  return <LuUser className='w-6 h-6 bg-primary rounded-full text-white' />;
 }
 
 export default UserIcon;

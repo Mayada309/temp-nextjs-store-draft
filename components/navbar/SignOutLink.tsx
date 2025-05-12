e13626cf815af8c4245d7d5625423608872b3024
Mayada <mayadaelsayed383@gmail.com>
2025-05-12 15:48:17 +0300

    add SignOutLink component with logout functionality and toast notification

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,5 +1,26 @@
+'use client';
+
+import { SignOutButton } from '@clerk/nextjs';
+import { useToast } from '../ui/use-toast';
+import Link from 'next/link';
+
 function SignOutLink() {
-  return <div>SignOutLink</div>;
+  const { toast } = useToast();
+  const handleLogout = () => {
+    toast({ description: 'Logout successfull!' });
+  };
+
+  return (
+    <SignOutButton>
+      <Link
+        href={'/'}
+        className='capitalize w-full text-left'
+        onClick={handleLogout}
+      >
+        logout
+      </Link>
+    </SignOutButton>
+  );
 }
 
 export default SignOutLink;
